d97e0df84df964ce826e1df2f78968f6981b2b60
KrishanNandan <krishannandan09@gmail.com>
2023-12-26 04:03:32 +0530

    statically serving file

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ const bodyParser = require("body-parser");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const rootDir = require("./utils/pathFinder");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+/**Hosting Css file staticaly */
+app.use(express.static(path.join(__dirname, "public")));
+
 /**Order matters */
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404-page.html"));
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(3000);
